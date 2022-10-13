45c1d520c7b3ec20081b644f245125575b09fb86
O Butt <60117081+o-butt@users.noreply.github.com>
2022-10-13 21:49:09 +0100

    Tidy comments

--- lambda.py
+++ lambda.py
@@ -1,66 +1,5 @@
 # imports
 import boto3
-
-# get the service resource
-dynamodb = boto3.resource('dynamodb')
-
-# define table to be working on set to variable "table"
-table = dynamodb.Table('visitorcounttable')
-
-
-response = table.get_item(
-    Key={
-        'record_id': 'lol' 
-        }
-)
-
-print("Response is: ", response)
-item = response['Item']
-print("\n")
-print("Item is:", item)
-print("\n")
-count = item['record_count']
-print("Count is currently: ", count)
-print("\n")
-
-##
-
-print("Count BEFORE update is", count)
-
-# increment count by 1
-table.update_item(
-    Key={
-        'record_id': 'lol',
-    },
-    UpdateExpression='SET record_count = record_count + :val1',
-    ExpressionAttributeValues={
-        ':val1': 1 
-    }
-)
-
-#### get count again
-response = table.get_item(
-    Key={
-        'record_id': 'lol'
-        }
-)
-#### set count again
-item = response['Item']
-count = item['record_count']
-
-print("Count AFTER update is", count)
-
-
-# visitor count on web page would get current count + 1 and show you that as you will be +1 visitor
-# TODO: turn this whole thing into some kind of function
-print("\n")
-print("You are visitor number: ", count)
-
-
-#############################---- lambda function working below ----#############################
-
-# imports
-import boto3
 import json
 
 # get the service resource
@@ -72,11 +11,11 @@ table = dynamodb.Table('visitorcounttable')
 def lambda_handler(event, context):
     response = table.get_item(
         Key={
-            'record_id': 'lol' 
+            'record_id': 'lol' # this record ID has already been set within DynamoDB
             }
     )
 
-    count = response['Item']['record_count']
+    count = response['Item']['record_count'] # pull out the actual count number from DynamoDB
     
     # increment count by 1 on DynamoDB table
     table.update_item(
@@ -89,14 +28,14 @@ def lambda_handler(event, context):
         }
     )
     
-    # get count again
+    # get count again after it's been incremented...
     response = table.get_item(
         Key={
             'record_id': 'lol'
             }
     )
     
-    # set count again
+    # ...so that you can set the count and return it
     count = response['Item']['record_count']
         
     return {
