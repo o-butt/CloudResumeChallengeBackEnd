2ce75f70960dc2055e9f1dd1836a592b567f54fc
O Butt <60117081+o-butt@users.noreply.github.com>
2022-10-23 19:31:41 +0100

    Remove non-backend type files as these have been moved to a different repo

--- script.js
+++
@@ -1,8 +0,0 @@
-console.log('wow, such visitors.');
-
-
-fetch('https://tomx98y9ib.execute-api.us-east-1.amazonaws.com/prod')
-    .then(response => response.json())
-    .then((data) => {
-        document.getElementById('idgoeshere').innerText = data.count
-    })
